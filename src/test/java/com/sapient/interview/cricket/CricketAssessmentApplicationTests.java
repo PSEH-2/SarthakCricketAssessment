042e4ea269f0e3a9f00e65af7294c78d6e30b4c5
PSEH-2 <39301737+PSEH-2@users.noreply.github.com>
2020-02-29 11:45:14 +0530

    Add files via upload

---
+++ src/test/java/com/sapient/interview/cricket/CricketAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapient.interview.cricket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
